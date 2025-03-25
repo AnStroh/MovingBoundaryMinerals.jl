94898c3fb3a07996eef569b24bedfb7f87a6abf7
Anstroh <a.stroh555@gmail.com>
2025-03-25 13:53:25 +0100

    New D1

--- examples/D1.jl
+++ examples/D1.jl
@@ -17,15 +17,21 @@ function main(plot_sim,verbose)
     #If you find a [] with two entries this belong to the respective side of
     #the diffusion couple ([left right])
     #Physics-----------------------------------------------------------------
-    Di          = [0.002   0.0004;]                                                                     #Initial diffusion coefficient in [m^2/s]; Approximated after Zhang & Cherniak (2010) p. 332 EQ. 19
-                                                                                                        #If you want to calculate D with the Arrhenius equation, set Di = [-1.0 -1.0;]
+    D0          = 5.38*1e-9                                                                             #Pre-exponential factor in [m^2/s]                                                                     
+    Ea          = 226000                                                                                #Activation energy for the left side in [J/mol]
+    alpha       = 90.0                                                                                  #Ideal angle between [001] and [010] (b-c-plane)
+    beta        = 90.0                                                                                  #Ideal angle between [001] and [100] (a-c-plane)
+    gamma       = 90.0                                                                                  #Ideal angle between [010] and [100] (a-b-plane)
+    deltaV      = 7*10^-6                                                                               #Volume change in [m^3/mol]
     Ri          = 2.0                                                                                   #Position of the interface -> initial radius of the left phase
     Tstart      = 1400.0 + 273.0                                                                        #Starting temperature in [K]
     Tstop       = 1300.0 + 273.0                                                                        #End temperature in [K]
+    P           = 10^6                                                                                  #Pressure in [Pa]
+    R           = 8.314472                                                                              #Universal gas constant in [J/(mol*K)]              
     Myr2Sec     = 60*60*24*365.25*1e6                                                                   #Conversion factor from Myr to s
-    t_tot       = 60*60*24                                                                                  #Total time [s]
+    t_tot       = 1e-3*Myr2Sec                                                                          #Total time [s]
     n           = 1                                                                                     #Geometry; 1: planar, 2: cylindrical, 3: spherical
-    CompInt     = 0.3                                                                                   #Composition of interest of the solid solution
+    CompInt     = 0.3                                                                                   #Composition of interest of the solid solution (Mg number)
     coeff       = readdlm("examples/Examples_phase_diagram/Coefficients_Reaction_lines.csv")            #Reads the coefficients for the linear least squares
     eq_values   = [coeff[1,1]  coeff[2,1]  coeff[3,1];	                                                #Coefficients for composition calculation of component B (stable at higher T) X2 = a2 + b2*T + c2*T²
                    coeff[1,2]  coeff[2,2]  coeff[3,2]]                                                  #Coefficients for composition calculation of component A (stable at lower T) X1 = a1 + b1*T + c1*T²
@@ -65,8 +71,13 @@ function main(plot_sim,verbose)
     #Preprocess and initial condition---------------------------------------
     t                   = 0.0                                                                           #Time
     it                  = 0                                                                             #Iterations
-    D_l                 = Di[1]                                                                         #Diffusion coefficient left comp.
-    D_r                 = Di[2]                                                                         #Diffusion coefficient right comp.
+    log10D_001          = log10(D0) - (Ea + (P-10^5)* deltaV)/(2.303*R*Tstart) + 3*((1-CompInt)-0.14)   #log10(diffusion coefficient Olivine) following Dohmen & Chakraborty (2007)                     
+    log10D_others       = log10D_001 - log10(6.0)                                                       #log10(diffusion coefficient Olivine) following Dohmen & Chakraborty (2007)                     
+    D_001               = 10^log10D_001                                                                 #Diffusion coefficient in direction 001
+    D_010               = 10^log10D_others                                                              #Diffusion coefficient in direction 010
+    D_100               = 10^log10D_others                                                              #Diffusion coefficient in direction 100
+    D_l                 = D_001*(cos(alpha))^2 + D_010*(cos(beta))^2 + D_100*(cos(gamma))^2             #Effective diffusion coefficient Olivine after Crank (1975), p. 7
+    D_r                 = exp(-7.92-26222/(Tstart))                                                     #Diffusivity melt: Approximated after Zhang & Cherniak (2010) p. 332 EQ. 19                   
     T                   = copy(Tstart)                                                                  #Initial temperature
     C_leftB, C_rightB   = composition(coeff_up,coeff_do,T)                                              #Initial composition of the phases
     Xc                  = (1.0 - CompInt) * C_rightB + CompInt * C_leftB                                #Actual total composition (Xc intersection with black dashed line, e.g. Xc = C(Ol)*V(Ol)+C(melt)*V(melt)
@@ -93,14 +104,14 @@ function main(plot_sim,verbose)
     Mass0               = calc_mass_vol(x_left,x_right,C_left,C_right,n,rho)                            #Initial mass
     Mass01              = (trapezoidal_integration(x_left.^n*rho[1],C_left)+ trapezoidal_integration(x_right.^n*rho[2],C_right))/Ri[2]    #Initial mass (plot)
     #Preallocate variables--------------------------------------------------
-    L_g         = spzeros(length(x0),length(x0))                                                        #Global LHS matrix
-    Mass        = Float64[]                                                                             #Array to store the mass of the system
-    Mass2       = Float64[]                                                                             #Array to store the mass of the system (plot)
-    KD_sim      = Float64[]                                                                             #Array to store the partition coefficient
-    T_sim       = Float64[]                                                                             #Array to store the temperature
-    R_left_sim  = Float64[]                                                                             #Array to store the concentration ratio left side (interface)
-    R_right_sim = Float64[]                                                                             #Array to store the concentration ratio right side (interface)
-    R_g         = zeros(length(x0),1)                                                                   #Global RHS vector
+    L_g                 = spzeros(length(x0),length(x0))                                                #Global LHS matrix
+    Mass                = Float64[]                                                                     #Array to store the mass of the system
+    Mass2               = Float64[]                                                                     #Array to store the mass of the system (plot)
+    KD_sim              = Float64[]                                                                     #Array to store the partition coefficient
+    T_sim               = Float64[]                                                                     #Array to store the temperature
+    R_left_sim          = Float64[]                                                                     #Array to store the concentration ratio left side (interface)
+    R_right_sim         = Float64[]                                                                     #Array to store the concentration ratio right side (interface)
+    R_g                 = zeros(length(x0),1)                                                           #Global RHS vector
     #-----------------------------------------------------------------------
     #Solving the moving boundary problem------------------------------------
     while t < t_tot
@@ -113,6 +124,14 @@ function main(plot_sim,verbose)
         C_left[end], C_right[1] = composition(coeff_up,coeff_do,T)
         dC  = C_right[1] - C_left[end]                                                                  #Composition difference
         rho = calculate_density(Xwm[:,1],Twm[1,:],rho_left,rho_right,C_leftB,C_rightB,T)
+        #Calculate diffusivities--------------------------------------------
+        log10D_001    = log10(D0) - (Ea + (P-10^5)* deltaV)/(2.303*R*T) + 3*((1-CompInt)-0.14)          #log10(diffusion coefficient Olivine) following Dohmen & Chakraborty (2007)                     
+        log10D_others = log10D_001 - log10(6.0)                                                         #log10(diffusion coefficient Olivine) following Dohmen & Chakraborty (2007)                     
+        D_001         = 10^log10D_001                                                                   #Diffusion coefficient in direction 001
+        D_010         = 10^log10D_others                                                                #Diffusion coefficient in direction 010
+        D_100         = 10^log10D_others                                                                #Diffusion coefficient in direction 100
+        D_l           = D_001*(cos(alpha))^2 + D_010*(cos(beta))^2 + D_100*(cos(gamma))^2               #Effective diffusion coefficient after Crank (1975), p. 7
+        D_r           = exp(-7.92-26222/(T))                                                            #Diffusivity melt: Approximated after Zhang & Cherniak (2010) p. 332 EQ. 19                   
         #Stefan condition -> Composition difference-------------------------
         JL   = - D_l * rho[1] * (C_left[end] - C_left[end-1]) * inv(dx1)                                #Flux of the left side to the right side
         JR   = - D_r * rho[2] * (C_right[2]  - C_right[1])    * inv(dx2)                                #Flux of the right side to the left side
@@ -157,33 +176,33 @@ function main(plot_sim,verbose)
             Tp_max = Tstart * 1.05
             first_val, last_val = values_between_known_indices!(Tlin,KDlin,Tstart,Tstop)                #CAUTION: Works just for constantly dropping temperature
             #Concentration profile
-            p1 = plot(x_left,C_left, lw=2, label=L"Left\ side")
-            p1 = plot!(x_right,C_right, lw=2, label=L"Right\ side")
-            p1 = plot!([x_left[end]; x_left[end]],[0; 1]*maxC,color=:grey,linestyle=:dash, lw=2, label=L"Interface")
-            p1 = plot!(x0,C0',color=:black,linestyle=:dash,xlabel = L"Distance", ylabel = L"Concentration", title = L"Concentration\ profile", lw=1.5,
-                  grid=:on, label=L"Initial\ condition",legend = :bottomright,legendfontsize = 6)
-            #Phase diagram
-            p2 = plot(Tlin,XC_left, lw=2, label=L"Left\ side")
-            p2 = plot!(Tlin,XC_right, lw=2, label=L"Right\ side")
-            p2 = scatter!([T],[C_left[end]],marker=:circle, markersize=2, markercolor=:black,
-                          markerstrokecolor=:black,label = "")
-            p2 = scatter!([T],[C_right[1]],marker=:circle, markersize=2, markercolor=:black,
-                          markerstrokecolor=:black,label = "")
-            p2 = plot!([T; T],[0; maximum([C_left[end],C_right[1]])],lw=1.5, label="",color=:black,linestyle=:dash)
-            p2 = plot!([T; 0],[C_left[end];C_left[end]],lw=1.5, label="",color=:royalblue,linestyle =:dot)
-            p2 = plot!([T; 0],[C_right[1];C_right[1]],lw=1.5, label="",xlims=(Tp_min, Tp_max), ylims=(0, 1),color=:crimson,linestyle =:dot)
-            p2 = plot!([Tstop*0.3; Tstart*1.5],[Mass2[end]; Mass2[end]],color=:dimgrey,linestyle=:dashdot,lw=1.5, label=L"Final\ mass")
-            p2 = plot!([Tstop*0.3; Tstart*1.5],[Mass01; Mass01],color=:grey,linestyle=:dashdot,lw=1.5, label=L"Initial\ mass",
-                        xlabel = L"Temperature", ylabel = L"X", title = L"Phase\ diagram",grid=:on,legendfontsize = 6)
-            #Evolution of KD(T)
-            p3 = plot(Tlin, KDlin, lw=2, label=L"Thermo-dynamic\ data", color=:teal)
-            p3 = scatter!([T_sim[end]],[KD_sim[end]],marker=:circle, markersize=2.5, markercolor=:black,markerstrokecolor=:black,
-                          xlabel = L"Temperature", ylabel = L"K_{D}", title = L"K_{D}(T)\ evolution", lw=1.5,
-                          grid=:on, label=L"Model",xlims=(Tp_min, Tp_max), ylims=(first_val-0.01,last_val+0.01),legendfontsize = 6)
-            #ln(KD) vs 1/T
-            p4 = plot(1.0 ./ T_sim,log.(KD_sim),xlabel = L"1/T", ylabel = L"ln(K_{D})", title = L"Arrhenius\ plot", lw=1.5,
-                    grid=:on, label=L"Model", color=:teal, ticks=:auto, xrotation=10,legendfontsize = 6)
-            p = plot(p2,p3,p4,p1,suptitle = L"Thermodynamical\ constrained\ Stefan\ condition")
+        p1 = plot(x_left,C_left, lw=2, label=L"Left\ side")
+        p1 = plot!(x_right,C_right, lw=2, label=L"Right\ side")
+        p1 = plot!([x_left[end]; x_left[end]],[0; 1]*maxC,color=:grey,linestyle=:dash, lw=2, label=L"Interface")
+        p1 = plot!(x0,C0,color=:black,linestyle=:dash,xlabel = L"Distance", ylabel = L"Concentration", title = L"Concentration\ profile", lw=1.5,
+              grid=:on, label=L"Initial\ condition",legendfontsize = 6,legend =:bottomright)
+        #Phase diagram
+        p2 = plot(Tlin,XC_left, lw=2, label=L"Left\ side")
+        p2 = plot!(Tlin,XC_right, lw=2, label=L"Right\ side")
+        p2 = scatter!([T],[C_left[end]],marker=:circle, markersize=2, markercolor=:black,
+                      markerstrokecolor=:black,label = "")
+        p2 = scatter!([T],[C_right[1]],marker=:circle, markersize=2, markercolor=:black,
+                      markerstrokecolor=:black,label = "")
+        p2 = plot!([T; T],[0; maximum([C_left[end],C_right[1]])],lw=1.5, label="",color=:black,linestyle=:dash)
+        p2 = plot!([T; 0],[C_left[end];C_left[end]],lw=1.5, label="",color=:royalblue,linestyle =:dot)
+        p2 = plot!([T; 0],[C_right[1];C_right[1]],lw=1.5, label="",xlims=(Tp_min, Tp_max), ylims=(0, 1),color=:crimson,linestyle =:dot)
+        p2 = plot!([Tstop*0.3; Tstart*1.5],[Mass2[end]; Mass2[end]],color=:dimgrey,linestyle=:dashdot,lw=1.5, label=L"Final\ mass")
+        p2 = plot!([Tstop*0.3; Tstart*1.5],[Mass01; Mass01],color=:grey,linestyle=:dashdot,lw=1.5, label=L"Initial\ mass",
+                    xlabel = L"Temperature", ylabel = L"X", title = L"Phase\ diagram",grid=:on,legendfontsize = 6,legend = :bottomright)
+        #Evolution of KD(T)
+        p3 = plot(Tlin, KDlin, lw=2, label=L"Thermo-dynamic\ data", color=:teal)
+        p3 = scatter!([T_sim[end]],[KD_sim[end]],marker=:circle, markersize=2.5, markercolor=:black,markerstrokecolor=:black,
+                      xlabel = L"Temperature", ylabel = L"K_{D}", title = L"K_{D}(T)\ evolution", lw=1.5,
+                      grid=:on, label=L"Model",xlims=(Tp_min, Tp_max), ylims=(first_val-0.01,last_val+0.01),legendfontsize = 6)
+        #ln(KD) vs 1/T
+        p4 = plot(1.0 ./ T_sim,log.(KD_sim),xlabel = L"1/T", ylabel = L"ln(K_{D})", title = L"Arrhenius\ plot", lw=1.5,
+                grid=:on, label=L"Model", color=:teal, ticks=:auto, xrotation=10,legendfontsize = 6)
+        plot(p2,p3,p4,p1,suptitle = L"Thermodynamical\ constrained\ Stefan\ condition", dpi = 300)
             display(p)
         end
     end
@@ -199,7 +218,7 @@ if run_and_plot
     plot_sim = false
     plot_end = true
     verbose  = false
-    save_file = true
+    save_file = false
     x_left, x_right, x0, C_left, C_right, C0, maxC, Tlin, XC_left, XC_right, T, Tstart, Tstop, KDlin, KD_sim,T_sim, Mass0, Mass, Mass01, Mass2 = main(plot_sim,verbose)
     if plot_end
         #Plotting-----------------------------------------------------------
@@ -234,8 +253,8 @@ if run_and_plot
         p4 = plot(1.0 ./ T_sim,log.(KD_sim),xlabel = L"1/T", ylabel = L"ln(K_{D})", title = L"Arrhenius\ plot", lw=1.5,
                 grid=:on, label=L"Model", color=:teal, ticks=:auto, xrotation=10,legendfontsize = 6)
         plot(p2,p3,p4,p1,suptitle = L"Thermodynamical\ constrained\ Stefan\ condition", dpi = 300)
-        save_path = "figures"
-        save_name = "D1"
-        save_figure(save_name,save_path,save_file)
+        #save_path = "figures"
+        #save_name = "D1"
+        #save_figure(save_name,save_path,save_file)
     end
 end
